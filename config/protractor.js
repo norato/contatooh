dcb0b8d374d145c5e30dc8682cfad5b719488382
Felipe Norato Lacerda <fnoratol@gmail.com>
2015-09-30 16:26:47 -0300

    Tests, but e2e is not working

---
+++ config/protractor.js
@@ -0,0 +1,12 @@
+exports.config = {
+  specs: ['../test/e2e/**/*.js'],
+  onPrepare: function() {
+    browser.driver.get('http://localhost:3000');
+    browser.driver.findElement(by.id('entrar')).click();
+    browser.driver.findElement(by.id('login_field'))
+      .sendKeys('gitHubUser');
+    browser.driver.findElement(by.id('password'))
+      .sendKeys('gitHubPassword');
+    browser.driver.findElement(by.name('commit')).click();  
+  }
+};
